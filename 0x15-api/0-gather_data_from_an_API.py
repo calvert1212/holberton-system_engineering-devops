9dedd0ca55fd8d84bbbb094646f77aa01f071f29
calvert1212 <calvert1212@gmail.com>
2021-06-17 03:55:57 +0000

    Fixing 0 still

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,31 +1,26 @@
 #!/usr/bin/python3
-""" Returns employee progress as formatted string """
-
+"""using REST API"""
 import requests
-from sys import argv
-
-
-if __name__ == "__main__":
-    
+import sys
+def get_employee_tasks(employeeId):
+    """get employee data"""
     name = ''
-    c = 0
     task_list = []
-    """ Gets the user and task data """
-    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                        .format(argv[1]))
-    todo = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
-                        .format(argv[1]))
-
-    name = user.json().get('name')
-    tasks = todo.json()
-    """ Checks if the tasks are completed """
-    for task in tasks:
+    c = 0
+    user = "https://jsonplaceholder.typicode.com/users/"
+    todo = "https://jsonplaceholder.typicode.com/users/"
+    userRes = requests.get("{}{}".format(user, employeeId))
+    todosRes = requests.get("{}{}/todos".format(todo, employeeId))
+    name = userRes.json().get('name')
+    todosJson = todosRes.json()
+    for task in todosJson:
         if task.get('completed') is True:
             c += 1
             task_list.append(task.get('title'))
-    """ Prints formated string """
-    print('Employee {} is done with tasks({}/{}):'
-          .format(name, c, len(tasks)))
-
-    for task in task_list:
-        print('\t {}'.format(task))
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, c, len(todosJson)))
+    for title in task_list:
+        print("\t {}".format(title))
+    return 0
+if __name__ == "__main__":
+    get_employee_tasks(sys.argv[1])
